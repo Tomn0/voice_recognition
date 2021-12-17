d86a4191101e8680d5386ae05a7f4c55c72a26b9
Tomn0 <nal.tomek@gmail.com>
2021-12-17 13:54:53 +0100

    added docstrings and comments

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import librosa
 import glob
 import os
 import pickle
+# import librosa.display
+from util_functions import display_mfccs, display_waveform, display_spectrogram
 
 
 # pomoc: https://towardsdatascience.com/extract-features-of-music-75a3f9bc265d
@@ -21,30 +23,34 @@ import pickle
 #                                     n_mfcc=n_mfcc, n_mels=n_mels,
 #                                     hop_length=hop_length,
 #                                     fmin=fmin, fmax=fmax, htk=False)
-
 # mfcc global params
-n_ftt=512
-hop_length=512
+n_ftt = 512
+hop_length = 512
 
-# librosa.util.example_audio_file()
-users = {}
-user_ids = os.listdir("data\\vox1_dev_wav_partaa_unzip")
-user_ids = user_ids[0:10]
-for user_id in user_ids:
-    sample = np.array([])
-    for file in glob.iglob(f"data\\vox1_dev_wav_partaa_unzip\\{user_id}\\**\\*.wav"):
-        y, sr = librosa.load(file)  # if error: add sr=sr
-        assert sr == 22050, f"wrong sampling rate for {file} - {sr}"
-        sample = np.append(sample, y)
-        # 200 * 22050
-        duration_in_seconds = float(len(sample) / sr)
-        if duration_in_seconds > 200:
-            sample = sample[:4410000]
-            break
-    sr = 22050
-    mfcc = librosa.feature.mfcc(sample, sr=sr, n_mfcc=24)
-    users[f'{user_id}'] = mfcc
-    
-len(users)
-pickle.dump(users, open("users1-10.p", "wb"))
+
+def main():
+
+    # librosa.util.example_audio_file()
+    users = {}
+    user_ids = os.listdir("data\\vox1_dev_wav_partaa_unzip")
+    user_ids = user_ids[0:10]
+    for user_id in user_ids:
+        sample = np.array([])
+        for file in glob.iglob(f"data\\vox1_dev_wav_partaa_unzip\\{user_id}\\**\\*.wav"):
+            y, sr = librosa.load(file)  # if error: add sr=sr
+            assert sr == 22050, f"wrong sampling rate for {file} - {sr}"
+            sample = np.append(sample, y)
+            # 200 * 22050
+            duration_in_seconds = float(len(sample) / sr)
+            if duration_in_seconds > 200:
+                sample = sample[:4410000]
+                break
+        sr = 22050
+        mfcc = librosa.feature.mfcc(sample, sr=sr, n_mfcc=24)
+        users[f'{user_id}'] = mfcc
+
+    len(users)
+    pickle.dump(users, open("users10.p", "wb"))
+
+main()
 
