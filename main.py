d5eba2c60ee07363edc5115c8b2362197958685b
Tomn0 <nal.tomek@gmail.com>
2022-01-04 19:19:37 +0100

    pipeline to calculate confidences

--- main.py
+++ main.py
@@ -1,63 +1,10 @@
-import numpy as np
-import librosa
-import librosa.display
-import glob
 import os
-import pickle
-import matplotlib.pylab as plt
-from util_functions import display_mfccs, display_waveform, display_spectrogram
 
-
-# pomoc: https://towardsdatascience.com/extract-features-of-music-75a3f9bc265d
-# MFCC params
-# dokładniejszy opis poszczególnych parametrów:
-# https://stackoverflow.com/questions/37963042/python-librosa-what-is-the-default-frame-size-used-to-compute-the-mfcc-feature
-# n_mfcc = 24
-# n_mels = 40
-# n_fft = 512
-# hop_length = 160
-# fmin = 0
-# fmax = None
-# sr = 16000
-# # ogranąc do czego są te wszystkie opcje
-# mfcc_librosa = librosa.feature.mfcc(y=y, sr=sr, n_fft=n_fft,
-#                                     n_mfcc=n_mfcc, n_mels=n_mels,
-#                                     hop_length=hop_length,
-#                                     fmin=fmin, fmax=fmax, htk=False)
-# mfcc global params
-n_ftt = 512
-hop_length = 512
-
+from generate_mfccs import mfccs_gen
 
 def main():
-
-    # librosa.util.example_audio_file()
-    users = {}
-    user_ids = os.listdir("data\\test")
-    user_ids = user_ids[0:512]
-    for user_id in user_ids:
-        sample = np.array([])
-        for file in glob.iglob(f"data\\test\\{user_id}\\**\\*.wav"):
-            y, sr = librosa.load(file)  # if error: add sr=sr
-            assert sr == 22050, f"wrong sampling rate for {file} - {sr}"
-            sample = np.append(sample, y)
-            # 200 * 22050
-            duration_in_seconds = float(len(sample) / sr)
-            if duration_in_seconds > 200:
-                sample = sample[:4410000]
-                break
-        sr = 22050
-        mfcc = librosa.feature.mfcc(sample, sr=sr, n_mfcc=13)
-        mfcc = mfcc[1:]     # pierwsza cecha to moc - nie jest nam potrzebna (czy na pewno?)
-
-        delta_mfcc = librosa.feature.delta(mfcc, axis=1)
-        delta2_mfcc = librosa.feature.delta(mfcc, order=2, axis=1)
-        mfccs = np.concatenate((mfcc, delta_mfcc, delta2_mfcc), axis=0)
-
-        users[f'{user_id}'] = mfccs
-    len(users)
-    pickle.dump(users, open("data/test.p", "wb"))
-
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    pass
 
 main()
 
--- training.py
+++ training.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pickle
+import matplotlib
 import matplotlib.pyplot as plt
 from keras.layers import Dense, Flatten
 from keras.models import Sequential
+from sklearn.metrics.pairwise import cosine_similarity
 
 # %%
 # loading train data
@@ -109,3 +111,170 @@ plt.show()
 # %%
 center_vector1 = model1.predict(X_train).mean(axis=0)
 # print(center_vector1.shape)
+models = {}
+models['model1'] = (model1, center_vector1)
+
+# util functions
+def create_enroll_and_test_set(eval_user, enroll_len=60, test_len=30):
+    slice_len = 5  # długość slice'a (5 sekund)
+    enroll_slices_num = round(enroll_len / slice_len)
+    test_slices_num = round(test_len / slice_len)
+
+    # przesuwamy index aby nie brać dwa razy tych samych danych
+    index_start = 0
+    inc_5s = round(slice_len * (eval_user.shape[1] / 200))  # 5 seconds increment added after each slice
+    enroll = np.empty(shape=(0, 7740))
+    test = np.empty(shape=(0, 7740))
+
+    index = round(slice_len * (eval_user.shape[1] / 200))
+
+    for _ in range(enroll_slices_num):
+        new_slice = eval_user[:, index_start:index].reshape(1, -1)
+        enroll = np.append(enroll, new_slice, axis=0)
+        index_start = index
+        index = index + inc_5s
+
+    for _ in range(test_slices_num):
+        new_slice = eval_user[:, index_start:index].reshape(1, -1)
+        test = np.append(test, new_slice, axis=0)
+        index_start = index
+        index = index + inc_5s
+
+    return enroll, test
+
+def centralize(vect, centr):
+  assert len(vect) == len(centr), f"{len(vect)} != {len(centr)}"
+
+  vect = vect - centr
+
+  return vect
+
+def calc_confidences(model_id):
+    # global model, center_vector
+    confidence_TP = []
+    confidence_TN = []
+    model = models[f'model{model_id}'][0]
+    center_vect = models[f'model{model_id}'][1]
+
+    # generowanie embeddingu
+    for userA in eval_data.keys():
+        enroll_A, test_A = create_enroll_and_test_set(eval_data[userA], enroll_len=60, test_len=30)
+
+        enroll_embedding_A = model.predict(enroll_A).mean(axis=0)
+        enroll_embedding_A = centralize(enroll_embedding_A, center_vect)
+
+        test_embedding_A = model.predict(test_A).mean(axis=0)
+        test_embedding_A = centralize(test_embedding_A, center_vect)
+
+        for userB in eval_data.keys():
+            if userA == userB:
+                continue
+
+            enroll_B, test_B = create_enroll_and_test_set(eval_data[userB], enroll_len=60, test_len=30)
+
+            enroll_embedding_B = model.predict(enroll_B).mean(axis=0)
+            enroll_embedding_B = centralize(enroll_embedding_B, center_vect)
+
+            test_embedding_B = model.predict(test_B).mean(axis=0)
+            test_embedding_B = centralize(test_embedding_B, center_vect)
+
+            # confidence_TP.append(cosine_similarity(enroll_embedding_A, test_embedding_A))
+            confidence_TP.append(
+                cosine_similarity(np.expand_dims(enroll_embedding_A, axis=0), np.expand_dims(test_embedding_A, axis=0)))
+
+            # confidence_TN.append(cosine_similarity(enroll_embedding_A, test_embedding_B))
+            confidence_TN.append(
+                cosine_similarity(np.expand_dims(enroll_embedding_A, axis=0), np.expand_dims(test_embedding_B, axis=0)))
+
+    confidence_TP = np.squeeze(np.array(confidence_TP))
+    confidence_TN = np.squeeze(np.array(confidence_TN))
+
+    return confidence_TP, confidence_TN
+
+
+# model evaluation
+confidence_TP, confidence_TN = calc_confidences(1)
+# confidence_TP_2, confidence_TN_2 = calc_confidences(2)
+# confidence_TP_3, confidence_TN_3 = calc_confidences(3)
+# confidence_TP_4, confidence_TN_4 = calc_confidences(4)
+
+scoring = {}
+scoring['model1'] = (confidence_TN.tolist(), confidence_TP.tolist())
+# scoring['model2'] = (confidence_TN_2.tolist(), confidence_TP_2.tolist())
+# scoring['model3'] = (confidence_TN_3.tolist(), confidence_TP_3.tolist())
+# scoring['model4'] = (confidence_TN_4.tolist(), confidence_TP_4.tolist())
+
+far_frr = {}
+far = []
+frr = []
+
+legend_score = ['score True Negative', 'score True Positive']
+legend_f = ['false acceptance rate', 'false rejection rate']
+plt.figure(figsize=(25, 10))
+for it, model in enumerate(scoring.keys()):
+  plt.subplot(2, len(scoring.keys()), it +1)
+  plt.title(model)
+  n_TN, bins, _ = plt.hist(scoring[model][0], bins=100, range=[-1, 1], alpha=0.5)
+  n_TP, bins, _ = plt.hist(scoring[model][1], bins=100, range=[-1, 1], alpha=0.5)
+  plt.grid()
+  plt.xlim([-1.5, 1.5])
+  plt.legend(legend_score, loc='upper right')
+  plt.xlabel('log likelihood')
+
+  # Implementacja False Rejection Rate / False Acceptance Rate
+  # Proszę uzupełnić słownik far_frr analogicznie do słownika scoring
+  # zamiast wartości score uzupełnić far, frr
+  # Miejsce na kod
+  # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+  far = 1 - (np.cumsum(n_TN) / np.sum(n_TN))
+  frr = np.cumsum(n_TP) / np.sum(n_TP)
+
+  far_frr[model] = (far, frr)
+
+
+  # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+  plt.subplot(2, len(scoring.keys()), it + 1 + len(scoring.keys()))
+  plt.plot(bins[1:], far_frr[model][0])
+  plt.plot(bins[1:], far_frr[model][1])
+  plt.legend(legend_f, loc='lower left')
+  plt.xlabel('threshold')
+  plt.ylabel('probability')
+  plt.grid()
+plt.show()
+
+def DETCurve(far_frr):
+    """
+    Given false positive and false negative rates, produce a DET Curve.
+    The false positive rate is assumed to be increasing while the false
+    negative rate is assumed to be decreasing.
+    """
+    plt.figure(figsize=(10, 10))
+    axis_min = 0
+    fig,ax = plt.subplots(figsize=(10, 10))
+    plt.yscale('log')
+    plt.xscale('log')
+    # ticks_to_use = [0.001,0.002,0.005,0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50]
+    ticks_to_use = [8, 15, 20, 27, 30, 50, 100]
+    ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.set_xticks(ticks_to_use)
+    ax.set_yticks(ticks_to_use)
+    legend_det = [dir]
+    for model in far_frr.keys():
+      (far, frr) = far_frr[model]
+      plt.plot(far*100,frr*100)
+    plt.axis([8,100,8,100])
+    plt.grid()
+    plt.legend(far_frr.keys(), loc='upper right')
+    plt.xlabel('false acceptance rate (%)')
+    plt.ylabel('false rejection rate (%)')
+    plt.show()
+
+DETCurve(far_frr)
+
+
+#########################
+#       RESULTS
+#########################
+
+import csv
