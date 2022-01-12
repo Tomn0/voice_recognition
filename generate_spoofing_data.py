9efa9662298ed611be653dcd5fffc1324ca6fa15
Tomn0 <nal.tomek@gmail.com>
2022-01-12 23:22:17 +0100

    ir_experiments - first template audio generation

--- generate_spoofing_data.py
+++ generate_spoofing_data.py
@@ -3,42 +3,23 @@ import librosa
 import os
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+import glob
 
-def spoof_():
-    users = {}
-    user_ids = os.listdir("data\\vox1_spoof")
-    user_ids = user_ids[0:512]
+# prepare audio to conv
+fs_ir, tmp_ir = wavfile.read(open("iPad_ir.wav", 'rb'))
+tmp_ir = tmp_ir[:500] / (2 ** 15)
 
-    fs, audio = wavfile.read('./person8.wav')
-    # print(fs)
-    ir = []
-    names = []
-    for filename in os.listdir("data\\vox1_spoof"):
-        if 'wav' in filename and 'conv' not in filename:
-            # print(filename)
-            fs, tmp_ir = wavfile.read('./'+filename)
-            tmp_ir = tmp_ir[:500] / (2**15)
-            ir.append(tmp_ir)
-            names.append(filename)
+# users = {}
+# user_ids = os.listdir("data\\test")
+# user_ids = user_ids[0:512]
+for filename in os.listdir(f"data\\vox1_spoof\\id10001\\7gWzIy6yIIk"):
+    if 'ir' not in filename and 'wav' in filename and 'conv' not in filename:
 
-    ir = np.array(ir)
-    plt.figure()
-    for it in range(ir.shape[0]):
-        """PLOTTING"""
-        plt.subplot(ir.shape[0], 2, 2*it+1)
-        time_domain = np.linspace(0, len(ir[it, :])/fs, len(ir[it, :]))
-        plt.plot(time_domain, ir[it, :])
-        plt.xlabel('time (s)')
+        fs, audio = wavfile.read("data\\vox1_spoof\\id10001\\7gWzIy6yIIk\\" + filename)
+
+        ir = np.array(tmp_ir)
 
         """CONVOLVING IMPULSE RESPONSE"""
-        convolved_data = np.convolve(audio, ir[it, :], mode='same')
-        time_domain = np.linspace(0, len(convolved_data)/fs, len(convolved_data))
-        plt.subplot(ir.shape[0], 2, 2*it+2)
-        plt.plot(time_domain, convolved_data)
-
+        convolved_data = np.convolve(audio, ir, mode='valid')
         convolved_data = convolved_data.astype(np.int16)
-
-        wavfile.write('conv_'+names[it], fs, convolved_data)
-
-    plt.show()
-
+        wavfile.write('conv_'+filename, fs_ir, convolved_data)
